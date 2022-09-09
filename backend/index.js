bb747a2f3eedcb53920aa1ba799b3ec84418d397
jeffrey mukkath <jeffreymukkath65@gmail.com>
2022-09-09 16:56:30 +0530

    DEPLOYING ONLINE

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const connect_db = require("./config/db");
 const dataRoute = require("./route/dataRoute");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,8 +11,16 @@ app.use(express.json());
 
 app.use("/api/data", dataRoute);
 
-app.get("/", (req, res) => {
-  res.send("API IS RUNNING");
-});
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API IS RUNNING");
+  });
+}
 
 app.listen(PORT, console.log(`SERVER IS RUNNING AT ${PORT}`));
